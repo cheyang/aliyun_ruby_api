848e65ae2f28dd58427f0741a687f6d49e57df68
cheyang <cheyang@cn.ibm.com>
2014-08-16 11:52:28 +0800

    add skeleton

---
+++ lib/aliyun_ruby_api/base.rb
@@ -0,0 +1,5 @@
+module Aliyun
+  class Base
+    API_ENDPOINT='https://ecs.aliyuncs.com/'
+  end
+end
---
+++ lib/aliyun_ruby_api/service.rb
@@ -0,0 +1,14 @@
+module Aliyun
+  class Service
+    
+    #The action to operate ECS, can be StartInstance
+    attr_accessor :action
+    
+    attr_accessor :endpoint_url
+    
+    def initialize(action, options = {})
+      
+    end
+    
+  end
+end
